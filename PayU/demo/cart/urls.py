32a459f32bcd3b937b6945a03f6b3e6c8144f14a
Alex Kasina <alexkasina1@gmail.com>
2015-09-25 18:16:35 -0400

    urlpatterns update in PayU/demo/cart/urls.py

--- PayU/demo/cart/urls.py
+++ PayU/demo/cart/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from cart import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^checkout/$', views.checkout, name='order.checkout'),
     url(r'^success/$', views.success, name='order.success'),
     url(r'^failure/$', views.failure, name='order.failure'),
     url(r'^cancel/$', views.cancel, name='order.cancel'),
-)
+]
